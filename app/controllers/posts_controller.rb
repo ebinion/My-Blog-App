0a5a900f3ffc78fad73be4258226d63d0c22de0f
Ezekiel Binion <ebinion@gmail.com>
2012-07-26 00:17:55 -0500

    Finsihed the post thumbnail

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,16 +22,16 @@ class PostsController < ApplicationController
   end
 
   def upload
-    # @post = Post.find params[:id]
+    @post = Post.find params[:id]
 
     uploaded_io = params[:post][:thumbnail]
 
-    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'w') do |file|
+    File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
       file.write(uploaded_io.read)
     end
 
-    # @post.thumbnail = Rails.root.join('public', 'uploads', uploaded_io.original_filename) + uploaded_io.original_filename
+    @post.thumbnail = uploaded_io.original_filename
 
-    # @post.save
+    @post.save
   end
 end
